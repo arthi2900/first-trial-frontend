69e5bb3a5bb6dd8910ece4e0705561cb1093c3cd
arthi2900 <aaarthi2927@gmail.com>
2022-06-25 07:43:52 -0700

    add

--- src/API.js
+++ src/API.js
@@ -7,8 +7,8 @@ export const APIposter = "https://62642a15a6adc673188e65dd.mockapi.io";
 export const APIcomment="https://62642a15a6adc673188e65dd.mockapi.io";
 
 */
-export const APIuser= "https://daymain.herokuapp.com";
+export const APIuser= "https://dayheroku.herokuapp.com";
 
-export const APIposter = "https://daymain.herokuapp.com";
+export const APIposter = "https://dayheroku.herokuapp.com";
 
-export const APIcomment="https://daymain.herokuapp.com";
+export const APIcomment="https://dayheroku.herokuapp.com";
